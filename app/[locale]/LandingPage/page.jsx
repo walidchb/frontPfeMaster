4d3554a2b1f3f83ee58297549a91fce857110016
ABDENOURGRT <gartiabdou074@gmail.com>
2024-03-07 00:10:38 +0100

    contactUs commit

--- app/[locale]/LandingPage/page.jsx
+++ app/[locale]/LandingPage/page.jsx
@@ -2,11 +2,13 @@ import React from "react";
 import Footer from "@/components/Footer";
 import NavBar from "@/components/NavBar";
 import SectionOneLandingPage from "@/components/SectionOneLandingPage";
+import ContactUs from "@/components/ContactUs";
 
 function LandingPage() {
   return (
     <div className="bg-red-600 w-screen h-screen">
       <NavBar />
+      <ContactUs />
       <SectionOneLandingPage />
       <Footer />
     </div>
