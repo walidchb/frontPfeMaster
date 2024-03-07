a1771a1900326ea3e772d4dc7a932dc438140143
ABDENOURGRT <gartiabdou074@gmail.com>
2024-03-07 18:46:21 +0100

    aboutus commit

--- app/[locale]/LandingPage/page.jsx
+++ app/[locale]/LandingPage/page.jsx
@@ -2,11 +2,13 @@ import React from "react";
 import Footer from "@/components/Footer";
 import NavBar from "@/components/NavBar";
 import SectionOneLandingPage from "@/components/SectionOneLandingPage";
+import AboutUs from "@/components/AboutUs";
 
 function LandingPage() {
   return (
     <div className="bg-red-600 w-screen h-screen">
       <NavBar />
+      <AboutUs />
       <SectionOneLandingPage />
       <Footer />
     </div>
