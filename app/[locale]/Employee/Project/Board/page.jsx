c39c949d5073a3287671e9bc06b4d43e07938d2b
ABDENOURGRT <gartiabdou074@gmail.com>
2024-06-07 04:37:26 +0100

    lot  commit

--- app/[locale]/Employee/Project/Board/page.jsx
+++ app/[locale]/Employee/Project/Board/page.jsx
@@ -1,23 +1,24 @@
 "use client";
 import React, { useRef } from "react";
 import "./style.css";
-
 import { Fragment, useState, useEffect } from "react";
 import MainProject from "@/components/Employee/Project/Main";
 import SideBarProject from "@/components/Employee/Project/SideBar";
 import SideBarEmployee from "@/components/Employee/SideBarEmployee";
 import ProtectedRoute from "@/components/ProtectedRoute";
-
 import BoardMain from "@/components/Employee/Project/Main/BoardMain/index";
 import KanbanBoard from "@/components/Employee/Project/Main/Kanban";
 import MainEmployee from "@/components/Employee/Main";
 import NavBarAuth from "@/components/NavBar/NavBarAuth";
 import MenuProject from "@/components/Employee/Project/MenuProject";
+import ProjectDetails from "@/components/Employee/Project/Main/DetailsMain";
+import ProgressCircle from "@/components/Employee/components/ProgressCercle";
+
 function Board() {
   const [showSideBar, setShowSideBar] = useState(true);
 
   return (
-    <div className=" bg-white text-black ">
+    <div className="bg-white text-black">
       <NavBarAuth
         className="flex-none"
         auth={true}
@@ -26,11 +27,12 @@ function Board() {
         showSideBar={showSideBar}
         setShowSideBar={setShowSideBar}
       />
-      <div style={{ height: "90vh" }} className=" flex flex-shrink-0 ">
+      <div style={{ height: "90vh" }} className="flex flex-shrink-0">
         {showSideBar ? <SideBarEmployee currentPage="Project" /> : null}
-
         <div className="w-full overflow-auto costumScrollBar">
           <MenuProject activePageIndex={0} />
+          
+          <ProjectDetails />
           <BoardMain />
         </div>
       </div>
